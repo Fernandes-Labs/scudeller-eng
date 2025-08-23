9dbb960290066e51fd6177a6448d71135808a006
José Fernandes <josefsjunior94@gmail.com>
2025-08-23 11:29:16 -0300

    add brand color pallete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
   return (
-    <div className="flex justify-center text-5xl">Olá, página Home</div>
+    <div className="flex flex-col items-center justify-center text-5xl gap-4">
+      <p className="text-blue">Olá, página Home</p>
+      <p className="text-gray-light">Olá, página Home</p>
+      <p className="text-gray-dark">Olá, página Home</p>
+      <p className="text-white">Olá, página Home</p>
+      <p className="text-yellow">Olá, página Home</p>
+    </div>
   );
 }
